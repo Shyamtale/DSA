3483d618602e7412e40f3702bce216afd3a1abb5
shyam_514 <talekarshyam06@gmail.com>
2025-06-26 17:42:17 +0530

    find by prefix sum

--- Array/prefixsumsub.java
+++ Array/prefixsumsub.java
@@ -1,24 +1,29 @@
-public class maxsubarrsum{
+public class prefixsumsub{
 
 public static void summ(int arr[]){
 int currsum=0;
 int maxsum=Integer.MIN_VALUE;
+int n=arr.length;
+ 
+
+// Step 1: Create prefix sum array
+int[] prefix = new int[n];
+prefix[0] = arr[0];
+for (int i = 1; i < n; i++) {
+    prefix[i] = prefix[i - 1] + arr[i];
+}
 
 for(int i=0;i<arr.length;i++){
     for(int j=i+1;j<arr.length;j++){
-        currsum=0;
-        for(int k=i;k<=j;k++){
-            
-          currsum+=arr[k] ;
-        }
-        System.out.print(currsum);
+        currsum=i==0?prefix[j]:prefix[j]-prefix[i-1];
+        
         if(maxsum<currsum){
             maxsum=currsum;
         }
         System.out.println();
     }
 }
-System.out.println("max sum is " +maxsum);
+System.out.println("max sum is "+maxsum);
 }
 
 public static void main(String args[]){
@@ -28,6 +33,6 @@ public static void main(String args[]){
 
 }
 
-//brute force 
-//brute force
+// prefix sum 
+
 
