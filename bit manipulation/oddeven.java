3ed3ab57092159bc852f18803fc22b413b82b89c
shyam_514 <talekarshyam06@gmail.com>
2025-07-08 23:25:04 +0530

    add some oproblems about bit manipulation

---
+++ bit manipulation/oddeven.java
@@ -0,0 +1,14 @@
+public class oddeven {
+    public static void oddeve(int n ){
+        int bitmask=1;
+        if((n & bitmask)==0){
+            //evennumber
+            System.out.println("even number");
+        }else{
+            System.out.println("odd number");
+        }
+    }
+    public static void main(String[] args) {
+        oddeve(3);
+    }
+}
