ac3004bfc9f0c1ee05278b89f276238ae84a832e
shyam_514 <talekarshyam06@gmail.com>
2025-08-03 23:08:19 +0530

    sell stock

---
+++ arraylllleetcode/stocks.java
@@ -0,0 +1,19 @@
+public class stocks {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price; 
+            } else {
+                int profit = price - minPrice; 
+                if (profit > maxProfit) {
+                    maxProfit = profit; 
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+}
