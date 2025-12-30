96732ccab1ef892c1ccd5267934149f273dbda84
Shyam Talekar <167209093+Shyamtale@users.noreply.github.com>
2025-12-30 22:33:07 +0530

    Refactor twoSum method and improve output formatting

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Brute force approach
+    
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
@@ -8,18 +8,16 @@ class Solution {
                 }
             }
         }
-        return new int[] {}; // Shouldn't reach here due to constraints
+        return new int[] {}; 
     }
 
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
 
-        // Create object of Solution to call instance method
         Solution sol = new Solution();
         int[] result = sol.twoSum(nums, target);
 
-        // Print result properly
         System.out.println("Output: [" + result[0] + ", " + result[1] + "]");
     }
 }
