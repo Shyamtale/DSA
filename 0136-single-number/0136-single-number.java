61b8ee1166f6eef98c4f61a53bd53df1ddd52780
Shyam Talekar <167209093+Shyamtale@users.noreply.github.com>
2025-12-31 17:32:29 +0530

    Time: 1 ms (99.87%), Space: 46.9 MB (44.82%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+      int ans=0;
+       for (int num:nums){
+        ans=ans^num;
+       }
+       return ans;
+    }
+}
